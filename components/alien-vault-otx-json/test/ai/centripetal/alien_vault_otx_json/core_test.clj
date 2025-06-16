6bc133a2627d98fe820fabfe503ad4d3cf8974c3
Damien Kick <damien.kick@vallen.com>
2025-06-15 19:27:14 -0500

    s/subject/sut/

--- components/alien-vault-otx-json/test/ai/centripetal/alien_vault_otx_json/core_test.clj
+++ components/alien-vault-otx-json/test/ai/centripetal/alien_vault_otx_json/core_test.clj
@@ -1,6 +1,6 @@
 (ns ai.centripetal.alien-vault-otx-json.core-test
   (:require
-    [ai.centripetal.alien-vault-otx-json.core :as subject]
+    [ai.centripetal.alien-vault-otx-json.core :as sut]
     [clojure.java.io :as io]
     [clojure.test :as test :refer [deftest is]]
     [jsonista.core :as json]
@@ -14,31 +14,33 @@
           io/resource
           slurp
           json/read-value
-          (as-> $ (every? subject/ioc-json-map? $)))))
+          (as-> $ (every? sut/ioc-json-map? $)))))
 
 (deftest ioc-map-test
-  (is (every? subject/ioc-map? (subject/make-ioc-seq))))
+  (is (every? sut/ioc-map? (sut/make-ioc-seq))))
 
 (deftest unique-ioc-ids-test
-  (is (subject/unique-ioc-map-ids? (subject/make-ioc-seq))))
+  (is (sut/unique-ioc-map-ids? (sut/make-ioc-seq))))
 
 (deftest duplicate-ioc-indicator-ids-test
-  (is (->> subject/ioc-seq
-           subject/ioc-indicator-id-frequencies
+  (is (->> sut/ioc-seq
+           sut/ioc-indicator-id-frequencies
            vals
            (some #(> % 1)))))
 
 (comment
-  (subject/ioc-indicator-id-frequencies subject/ioc-seq)
+  (sut/ioc-indicator-id-frequencies sut/ioc-seq)
 
+  (sut/get-indicators)
+  
   (def ids [700790756 2437645 888830013])
 
-  (subject/get-ioc-indicators--id 700790756)
-  (subject/get-ioc-indicators--id 2437645)
-  (subject/get-ioc-indicators--id 888830013)
+  (sut/get-ioc-indicators--id 700790756)
+  (sut/get-ioc-indicators--id 2437645)
+  (sut/get-ioc-indicators--id 888830013)
 
-  (subject/get-indicators--id 2437645)
-  (subject/get-indicators--id 888830013)
+  (sut/get-indicators--id 2437645)
+  (sut/get-indicators--id 888830013)
   
   (-> "alien-vault-otx-json/indicators.json"
       io/resource
@@ -49,6 +51,6 @@
   (-> "alien-vault-otx-json/indicators.json"
       io/resource
       slurp
-      (json/read-value subject/mapper)
+      (json/read-value sut/mapper)
       mp/provide)
   #_|)
